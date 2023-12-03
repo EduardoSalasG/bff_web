660f11caa02dbd812df098f8ee91270c0c155c21
MariaMaiese <maria.maiese@correoaiep.cl>
2023-12-03 08:36:04 -0300

    rehaciendo todo el proyecto con graphql

---
+++ dist/index.js
@@ -0,0 +1,39 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import cors from 'cors';
+import express from 'express';
+const app = express();
+const typeDefs = `#graphql
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+const books = [
+    {
+        title: 'The Awakening',
+        author: 'Kate Chopin',
+    },
+    {
+        title: 'City of Glass',
+        author: 'Paul Auster',
+    },
+];
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+};
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+await server.start();
+app.use('/graphql', cors(), express.json(), expressMiddleware(server));
+app.listen({ port: 4000 }, () => {
+    console.log(`🚀  Server ready at: http://localhost:4000/graphql`);
+});
--- graphql/models.ts
+++
@@ -1,7 +0,0 @@
-import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
-
-
-type User = {
-    id: ID
-    name: String
-}

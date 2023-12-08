d1f6d86ab506102b61544766f84990c5e3e866de
MariaMaiese <maria.maiese@correoaiep.cl>
2023-12-08 16:14:05 -0300

    agregando schemas y resolvers de todas las bases de datos

--- dist/schema.js
+++ dist/schema.js
@@ -1,4 +1,5 @@
 import { gql } from "apollo-server-express";
+//FIXME: agregar tabla intermedia de tipousuario_usuario
 const typeDefs = gql `
 
   type TipoUsuario {
@@ -17,14 +18,231 @@ const typeDefs = gql `
     USU_GENERO: Boolean!
     USU_CORREO: String!
     USU_CONTRASENA: String!
+    TipoUsuario: [TipoUsuario!]!
   }
 
+  type PreguntaSeguridad {
+    PSE_ID: ID!
+    PSE_ENUNCIADO: String!
+  }
 
+  type Cotizacion{
+    COT_ID: ID!
+    COT_FECHA: String!
+    COT_RESPONDIDA: Boolean!
+    COT_FECHA_RESPUESTA: String!
+    COT_MENSAJE: String!
+    COT_CANTIDAD_PARTICIPANTES: Int!
+    CUR_ID: Int!
+    USU_ID: Int!
+  }
+
+  type EstadoTransaccion{
+    ETR_ID: ID!
+    ETR_NOMBRE: String!
+  }
+
+  type EstadoVenta{
+    EVE_ID: ID!
+    EVE_NOMBRE: String!
+  }
+
+  type MetodoPago{
+    MPA_ID: ID!
+    MPA_NOMBRE: String!
+  }
+
+  type TipoDt{
+    TDT_ID: ID!
+    TDT_NOMBRE: String!
+  }
+
+  type Transaccion{
+    TRA_ID: ID!
+    TRA_FECHA: String!
+    ETR_ID: Int!
+    EstadoTransaccion: EstadoTransaccion!
+    MPA_ID:Int!
+    MetodoPago: MetodoPago!
+    VEN_ID:Int!
+    Venta: Venta!
+  }
+
+  type Venta{
+    VEN_ID:ID!
+    VEN_MONTO: Int!
+    VEN_RUT_DT: String!
+    EVE_ID:Int!
+    EstadoVenta: EstadoVenta!
+    TDT_ID:Int!
+    TipoDt: TipoDt!
+    USU_ID:Int!
+    Usuario: Usuario!
+    PRO_ID:Int!
+    Producto: Producto!
+  }
+
+  type CategoriaCurso{
+    CCU_ID: ID!
+    CCU_NOMBRE: String!
+  }
+
+  type CategoriaLead{
+    CLE_ID: ID!
+    CLE_NOMBRE: String!
+  }
+
+  type CursoProducto{
+    PRO_ID: ID!
+    Curso: Producto!
+    CUR_DIRIGIDO_A: String!
+    CUR_OBJETIVOS: String!
+    CUR_ESTRUCTURA: String!
+    CUR_IMAGEN_1: String!
+    CUR_IMAGEN_2: String!
+    CUR_VIDEO_PROMOCIONAL: String!
+    CUR_FECHA_INICIO: String!
+    CUR_CANTIDAD_MIN_PARTICIPANTES: Int!
+    CUR_CANTIDAD_MAX_PARTICIPANTES: Int!
+    CUR_CODIGO_SENCE: String!
+    CUR_DURACION: Int!
+    CUR_INCLUYE_CERTIFICACION: Boolean!
+    USU_ID: Int!
+    Usuario: Usuario!
+    TDU_ID: Int!
+    TipoDeDuracion: TipoDeDuracion
+    MOD_ID: Int!
+    Modalidad: Modalidad!
+    SCU_ID:Int!
+    SubcategoriaCurso: SubcategoriaCurso!
+  }
+
+  type LeadProducto{
+    PRO_ID: ID!
+    Curso: Producto!
+    LEA_URL_DIR: String!
+    SLE_ID: Int!
+    SubcategoriaLead: SubcategoriaLead!
+  }
+
+  type Modalidad {
+    MOD_ID: ID!
+    MOD_NOMBRE: String!
+  }
+
+  type Producto{
+    PRO_ID: ID!
+    PRO_NOMBRE: String!
+    PRO_DESCRIPCION: String!
+    PRO_DESCRIPCION_CORTA: String!
+    PRO_PRECIO: Int!
+    PRO_PRECIO_DESCUENTO: Int!
+    PRO_DESTACADO: Boolean!
+  }
+
+  type Promocion{
+    PRM_ID: ID!
+    PRM_NOMBRE: String!
+    PRM_DESCUENTO: Int!
+    PRM_CANTIDAD_PARTICIANTES: Int!
+    PRO_ID: Int!
+    Producto: Producto!
+  }
+
+  type SubcategoriaCurso{
+    SCU_ID: ID!
+    SCU_NOMBRE: String!
+    CCU_ID:Int!
+    CategoriaCurso: CategoriaCurso!
+  }
+
+  type SubcategoriaLead{
+    SLE_ID: ID!
+    SLE_NOMBRE: String!
+    CLE_ID:Int!
+    CategoriaLead: CategoriaLead!
+  }
+
+  type TipoDeDuracion{
+    TDU_ID: ID!
+    TDU_NOMBRE: String!
+    TDU_DESCRIPCION: String!
+  }
+
+  type Valoracion{
+    VAL_ID: ID!
+    VAL_FECHA: String!
+    VAL_PUNTAJE: Float!
+    VAL_COMENTARIO: String!
+    USU_ID:Int!
+    Usuario: Usuario!
+    PRO_ID:Int!
+    Producto:Producto!
+  }
   type Query{
-    tipo_usuarioById(TUS_ID:ID): TipoUsuario
-    todosTiposUsuarios: [TipoUsuario]
-    usuario_ById(USU_ID:ID) : Usuario
+    getTipoUsuarioById(TUS_ID:ID): TipoUsuario
+    getTiposUsuarios: [TipoUsuario]
+
+    getUsuarioById(USU_ID:ID) : Usuario
     getUsuarios: [Usuario]
+    
+    getPreguntasSeguridad: [PreguntaSeguridad]
+    getPreguntaSeguridadById(PSE_ID:ID): PreguntaSeguridad
+    
+    getCotizaciones:[Cotizacion]
+    getCotizacionById(COT_ID:ID): Cotizacion
+    
+    getEstadosTransaccion:[EstadoTransaccion]
+    getEstadoTransaccionById(EVE_ID:ID): EstadoTransaccion
+    
+    getEstadosVenta:[EstadoVenta]
+    getEstadoVentaById(COT_ID:ID): EstadoVenta
+
+    getMetodosPago:[MetodoPago]
+    getMetodoPagoById(MPA_ID:ID): MetodoPago
+
+    getTiposDt:[TipoDt]
+    getTipoDtById(TDT_ID:ID): TipoDt
+
+    getTransacciones:[Transaccion]
+    getTransaccionById(TRA_ID:ID): Transaccion
+
+    getVentas:[Venta]
+    getVentaById(VEN_ID:ID): Venta
+    
+    getCategoriasCurso:[CategoriaCurso]
+    getCategoriaCursoById(CCU_ID:ID): CategoriaCurso
+
+    getCategoriasLead:[CategoriaLead]
+    getCategoriaLeadById(CLE_ID:ID): CategoriaLead
+
+    getCursosProducto:[CursoProducto]
+    getCursoProductoById(PRO_ID:ID): CursoProducto
+
+    getLeadsProducto:[LeadProducto]
+    getLeadProductoById(PRO_ID:ID): LeadProducto
+
+    getModalidades:[Modalidad]
+    getModalidadById(MOD_ID:ID): Modalidad
+
+    getProductos:[Producto]
+    getProductoById(PRO_ID:ID): Producto
+
+    getPromociones:[Promocion]
+    getPromocionById(PRM_ID:ID): Promocion
+
+    getSubcategoriasCurso:[SubcategoriaCurso]
+    getSubcategoriaCursoById(SCU_ID:ID): SubcategoriaCurso
+
+    getSubcategoriasLead:[SubcategoriaLead]
+    getSubcategoriaLeadById(SLE_ID:ID): SubcategoriaLead
+
+    getTiposDeDuracion:[TipoDeDuracion]
+    getTipoDeDuracionById(TDU_ID:ID): TipoDeDuracion
+
+    getValoraciones:[Valoracion]
+    getValoracionById(VAL_ID:ID): Valoracion
   }
 `;
+//FIXME: campos que son date
 export { typeDefs };
--- resolvers.ts
+++ resolvers.ts
@@ -2,26 +2,22 @@ import axios from 'axios';
 
 const resolvers = {
     Query: {
-        tipo_usuarioById: async (parent: any, { TUS_ID }) => {
+        getTipoUsuarioById: async (parent: any, { TUS_ID }) => {
             const tipoUsuarioId = TUS_ID
             const response = await axios.get(`http://localhost:8080/tipo_usuarios/${tipoUsuarioId}`)
             const tipo_usuarioData = response.data.body;
-            console.log(response.data)
-
             return tipo_usuarioData
         },
-        todosTiposUsuarios: async (root: any) => {
+        getTiposUsuarios: async (root: any) => {
             const response = await axios.get(`http://localhost:8080/tipo_usuarios/`)
             const allTipoUsuarios = response.data.body;
             return allTipoUsuarios
 
         },
-        usuario_ById: async (parent: any, { USU_ID }) => {
+        getUsuarioById: async (parent: any, { USU_ID }) => {
             const usuarioId = USU_ID
             const response = await axios.get(`http://localhost:8080/usuarios/${usuarioId}`)
             const usuarioData = response.data.body;
-            console.log(response.data)
-
             return usuarioData
         },
         getUsuarios: async (root: any) => {
@@ -42,7 +38,360 @@ const resolvers = {
             const preguntaSeguridadData = response.data.body;
             return preguntaSeguridadData
         },
+        getCotizaciones: async () => {
+            const response = await axios.get(`http://localhost:8081/cotizaciones/`)
+            const allCotizaciones = response.data.body;
+            return allCotizaciones
 
+        },
+        getCotizacionById: async (parent: any, { COT_ID }) => {
+            const cotizacionId = COT_ID
+            const response = await axios.get(`http://localhost:8081/cotizaciones/${cotizacionId}`)
+            const cotizacionData = response.data.body;
+            return cotizacionData
+        },
+        getEstadosTransaccion: async () => {
+            const response = await axios.get(`http://localhost:8081/estados-transaccion/`)
+            const allEstadosTransaccion = response.data.body;
+            return allEstadosTransaccion
+
+        },
+        getEstadoTransaccionById: async (parent: any, { ETR_ID }) => {
+            const estadoTransaccionId = ETR_ID
+            const response = await axios.get(`http://localhost:8081/estados-transaccion/${estadoTransaccionId}`)
+            const estadoTransaccionData = response.data.body;
+            return estadoTransaccionData
+        },
+        getEstadosVenta: async () => {
+            const response = await axios.get(`http://localhost:8081/estados-venta/`)
+            const allEstadosVenta = response.data.body;
+            return allEstadosVenta
+
+        },
+        getEstadoVentaById: async (parent: any, { EVE_ID }) => {
+            const estadoVentaId = EVE_ID
+            const response = await axios.get(`http://localhost:8081/estados-venta/${estadoVentaId}`)
+            const estadoVentaData = response.data.body;
+            return estadoVentaData
+        },
+        getMetodosPago: async () => {
+            const response = await axios.get(`http://localhost:8081/metodos-pago/`)
+            const allMetodosPago = response.data.body;
+            return allMetodosPago
+
+        },
+        getMetodoPagoById: async (parent: any, { MPA_ID }) => {
+            const metodoPagoId = MPA_ID
+            const response = await axios.get(`http://localhost:8081/metodos-pago/${metodoPagoId}`)
+            const metodoPagoData = response.data.body;
+            return metodoPagoData
+        },
+        getTiposDt: async () => {
+            const response = await axios.get(`http://localhost:8081/tipos-dt/`)
+            const allTiposDt = response.data.body;
+            return allTiposDt
+
+        },
+        getTipoDtById: async (parent: any, { TDT_ID }) => {
+            const tipoDtId = TDT_ID
+            const response = await axios.get(`http://localhost:8081/tipos-dt/${tipoDtId}`)
+            const tipoDtData = response.data.body;
+            return tipoDtData
+        },
+        getTransacciones: async () => {
+            const response = await axios.get(`http://localhost:8081/transacciones/`)
+            const allTransacciones = response.data.body;
+            return allTransacciones
+
+        },
+        getTransaccionById: async (parent: any, { TRA_ID }) => {
+            const transaccionId = TRA_ID
+            const response = await axios.get(`http://localhost:8081/transacciones/${transaccionId}`)
+            const transaccionData = response.data.body;
+            return transaccionData
+        },
+        getVentas: async () => {
+            const response = await axios.get(`http://localhost:8081/ventas/`)
+            const allVentas = response.data.body;
+            return allVentas
+
+        },
+        getVentaById: async (parent: any, { VEN_ID }) => {
+            const ventaId = VEN_ID
+            const response = await axios.get(`http://localhost:8081/ventas/${ventaId}`)
+            const ventaData = response.data.body;
+            return ventaData
+        },
+        getCategoriasCurso: async () => {
+            const response = await axios.get(`http://localhost:8082/categorias-curso/`)
+            const allCategoriasCurso = response.data.body;
+            return allCategoriasCurso
+
+        },
+        getCategoriaCursoById: async (parent: any, { CCU_ID }) => {
+            const categoriaCursoId = CCU_ID
+            const response = await axios.get(`http://localhost:8082/categorias-curso/${categoriaCursoId}`)
+            const categoriaCursoData = response.data.body;
+            return categoriaCursoData
+        },
+        getCategoriasLead: async () => {
+            const response = await axios.get(`http://localhost:8082/categorias-lead/`)
+            const allCategoriasLead = response.data.body;
+            return allCategoriasLead
+
+        },
+        getCategoriaLeadById: async (parent: any, { CLE_ID }) => {
+            const categoriaLeadId = CLE_ID
+            const response = await axios.get(`http://localhost:8082/categorias-lead/${categoriaLeadId}`)
+            const categoriaLeadData = response.data.body;
+            return categoriaLeadData
+        },
+        getCursosProducto: async () => {
+            const response = await axios.get(`http://localhost:8082/cursos/`)
+            const allCursosProducto = response.data.body;
+            return allCursosProducto
+
+        },
+        getCursoProductoById: async (parent: any, { PRO_ID }) => {
+            const cursoProductoId = PRO_ID
+            const response = await axios.get(`http://localhost:8082/cursos/${cursoProductoId}`)
+            const cursoProductoData = response.data.body;
+            return cursoProductoData
+        },
+        getLeadsProducto: async () => {
+            const response = await axios.get(`http://localhost:8082/leads/`)
+            const allLeadsProducto = response.data.body;
+            return allLeadsProducto
+
+        },
+        getLeadProductoById: async (parent: any, { PRO_ID }) => {
+            const leadProductoId = PRO_ID
+            const response = await axios.get(`http://localhost:8082/leads/${leadProductoId}`)
+            const leadProductoData = response.data.body;
+            return leadProductoData
+        },
+        getModalidades: async () => {
+            const response = await axios.get(`http://localhost:8082/modalidades/`)
+            const allModalidades = response.data.body;
+            return allModalidades
+
+        },
+        getModalidadById: async (parent: any, { MOD_ID }) => {
+            const modalidadId = MOD_ID
+            const response = await axios.get(`http://localhost:8082/modalidades/${modalidadId}`)
+            const modalidadData = response.data.body;
+            return modalidadData
+        },
+        getProductos: async () => {
+            const response = await axios.get(`http://localhost:8082/productos/`)
+            const allProductos = response.data.body;
+            return allProductos
+
+        },
+        getProductoById: async (parent: any, { PRO_ID }) => {
+            const productoId = PRO_ID
+            const response = await axios.get(`http://localhost:8082/productos/${productoId}`)
+            const productoData = response.data.body;
+            return productoData
+        },
+        getPromociones: async () => {
+            const response = await axios.get(`http://localhost:8082/promociones/`)
+            const allPromociones = response.data.body;
+            return allPromociones
+
+        },
+        getPromocionById: async (parent: any, { PRM_ID }) => {
+            const promocionId = PRM_ID
+            const response = await axios.get(`http://localhost:8082/promociones/${promocionId}`)
+            const promocionData = response.data.body;
+            return promocionData
+        },
+        getSubcategoriasCurso: async () => {
+            const response = await axios.get(`http://localhost:8082/subcategorias-curso/`)
+            const allSubcategoriasCurso = response.data.body;
+            return allSubcategoriasCurso
+
+        },
+        getSubcategoriaCursoById: async (parent: any, { SCU_ID }) => {
+            const subcategoriaCursoId = SCU_ID
+            const response = await axios.get(`http://localhost:8082/subcategorias-curso/${subcategoriaCursoId}`)
+            const subcategoriaCursoData = response.data.body;
+            return subcategoriaCursoData
+        },
+        getSubcategoriasLead: async () => {
+            const response = await axios.get(`http://localhost:8082/subcategorias-lead/`)
+            const allSubcategoriasLead = response.data.body;
+            return allSubcategoriasLead
+
+        },
+        getSubcategoriaLeadById: async (parent: any, { SLE_ID }) => {
+            const subcategoriaLeadId = SLE_ID
+            const response = await axios.get(`http://localhost:8082/subcategorias-lead/${subcategoriaLeadId}`)
+            const subcategoriaLeadData = response.data.body;
+            return subcategoriaLeadData
+        },
+        getTiposDeDuracion: async () => {
+            const response = await axios.get(`http://localhost:8082/tipos-duracion/`)
+            const allTiposDuracion = response.data.body;
+            return allTiposDuracion
+
+        },
+        getTipoDeDuracionById: async (parent: any, { TDU_ID }) => {
+            const tipoDuracionId = TDU_ID
+            const response = await axios.get(`http://localhost:8082/tipos-duracion/${tipoDuracionId}`)
+            const tipoDuracionData = response.data.body;
+            return tipoDuracionData
+        },
+        getValoraciones: async () => {
+            const response = await axios.get(`http://localhost:8082/valoraciones/`)
+            const allValoraciones = response.data.body;
+            return allValoraciones
+
+        },
+        getValoracionById: async (parent: any, { TDU_ID }) => {
+            const valoracionId = TDU_ID
+            const response = await axios.get(`http://localhost:8082/valoraciones/${valoracionId}`)
+            const valoracionData = response.data.body;
+            return valoracionData
+        },
+    },
+    CursoProducto: {
+        Curso: async (parent: any) => {
+            const productoId = parent.PRO_ID
+            const response = await axios.get(`http://localhost:8082/productos/${productoId}`)
+            const productoData = response.data.body;
+            return productoData
+        },
+        Usuario: async (parent: any) => {
+            const usuarioId = parent.USU_ID
+            const response = await axios.get(`http://localhost:8080/usuarios/${usuarioId}`)
+            const usuarioData = response.data.body;
+            return usuarioData
+        },
+        TipoDeDuracion: async (parent: any) => {
+            const tipoDuracionId = parent.TDU_ID;
+            const response = await axios.get(`http://localhost:8082/tipos-duracion/${tipoDuracionId}`);
+            const tipoDuracionData = response.data.body;
+            return tipoDuracionData;
+        },
+        Modalidad: async (parent: any) => {
+            const modalidadId = parent.MOD_ID
+            const response = await axios.get(`http://localhost:8082/modalidades/${modalidadId}`)
+            const modalidadData = response.data.body;
+            return modalidadData
+        },
+        SubcategoriaCurso: async (parent: any) => {
+            const subcategoriaCursoId = parent.SCU_ID
+            const response = await axios.get(`http://localhost:8082/subcategorias-curso/${subcategoriaCursoId}`)
+            const subcategoriaCursoData = response.data.body;
+            return subcategoriaCursoData
+        }
+    },
+    Usuario: {
+        TipoUsuario: async (parent: any) => {
+            const usuarioId = parent.USU_ID
+            const getTipoUsuarioById = await axios.get(`http://localhost:8080/tipo_usuario_usuarios/get-by-user/${usuarioId}`)
+            const tipo_usuarioData = getTipoUsuarioById.data.body;
+
+            const tipoUsuarioInfoPromises = tipo_usuarioData.map(async (tipoUsuario) => {
+                const tipoUsuarioId = tipoUsuario.TUS_ID;
+                const response = await axios.get(`http://localhost:8080/tipo_usuarios/${tipoUsuarioId}`);
+                return response.data.body;
+            });
+            return tipoUsuarioInfoPromises
+        }
+    },
+    Transaccion: {
+        EstadoTransaccion: async (parent: any) => {
+            const estadoTransaccionId = parent.ETR_ID
+            const response = await axios.get(`http://localhost:8081/estados-transaccion/${estadoTransaccionId}`)
+            const estadoTransaccionData = response.data.body;
+            return estadoTransaccionData
+        },
+        MetodoPago: async (parent: any) => {
+            const metodoPagoId = parent.MPA_ID
+            const response = await axios.get(`http://localhost:8081/metodos-pago/${metodoPagoId}`)
+            const metodoPagoData = response.data.body;
+            return metodoPagoData
+        },
+        Venta: async (parent: any) => {
+            const ventaId = parent.VEN_ID
+            const response = await axios.get(`http://localhost:8081/ventas/${ventaId}`)
+            const ventaData = response.data.body;
+            return ventaData
+        }
+    },
+    Venta: {
+        EstadoVenta: async (parent: any) => {
+            const estadoVentaId = parent.EVE_ID
+            const response = await axios.get(`http://localhost:8081/estados-venta/${estadoVentaId}`)
+            const estadoVentaData = response.data.body;
+            return estadoVentaData
+        },
+        TipoDt: async (parent: any) => {
+            const tipoDtId = parent.TDT_ID
+            const response = await axios.get(`http://localhost:8081/tipos-dt/${tipoDtId}`)
+            const tipoDtData = response.data.body;
+            return tipoDtData
+        },
+        Usuario: async (parent: any) => {
+            const usuarioId = parent.USU_ID
+            const response = await axios.get(`http://localhost:8080/usuarios/${usuarioId}`)
+            const usuarioData = response.data.body;
+            return usuarioData
+        }
+    },
+    LeadProducto: {
+        Curso: async (parent: any) => {
+            const productoId = parent.PRO_ID
+            const response = await axios.get(`http://localhost:8082/productos/${productoId}`)
+            const productoData = response.data.body;
+            return productoData
+        },
+        SubcategoriaLead: async (parent: any) => {
+            const subcategoriaLeadId = parent.SLE_ID
+            const response = await axios.get(`http://localhost:8082/subcategorias-lead/${subcategoriaLeadId}`)
+            const subcategoriaLeadData = response.data.body;
+            return subcategoriaLeadData
+        }
+    },
+    Promocion: {
+        Producto: async (parent: any) => {
+            const productoId = parent.PRO_ID
+            const response = await axios.get(`http://localhost:8082/productos/${productoId}`)
+            const productoData = response.data.body;
+            return productoData
+        }
+    },
+    SubcategoriaCurso: {
+        CategoriaCurso: async (parent: any) => {
+            const categoriaCursoId = parent.CCU_ID
+            const response = await axios.get(`http://localhost:8082/categorias-curso/${categoriaCursoId}`)
+            const categoriaCursoData = response.data.body;
+            return categoriaCursoData
+        }
+    },
+    SubcategoriaLead: {
+        CategoriaLead: async (parent: any) => {
+            const categoriaLeadId = parent.CLE_ID
+            const response = await axios.get(`http://localhost:8082/categorias-lead/${categoriaLeadId}`)
+            const categoriaLeadData = response.data.body;
+            return categoriaLeadData
+        }
+    },
+    Valoracion: {
+        Usuario: async (parent: any) => {
+            const usuarioId = parent.USU_ID
+            const response = await axios.get(`http://localhost:8080/usuarios/${usuarioId}`)
+            const usuarioData = response.data.body;
+            return usuarioData
+        },
+        Producto: async (parent: any) => {
+            const productoId = parent.PRO_ID
+            const response = await axios.get(`http://localhost:8082/productos/${productoId}`)
+            const productoData = response.data.body;
+            return productoData
+        }
     }
 }
 export { resolvers };
